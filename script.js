f3dfbdd2ea2639b2e807e3ea6246525bdd0101e2
sohail-khan880 <116253077+sohail-khan880@users.noreply.github.com>
2024-11-11 06:28:09 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,209 @@
+"use strick";
+/*
+function showAlert() {
+  alert("Log In Not Working Yet!");
+}
+function Booking() {
+  alert("Booking Is Not Available");
+}
+
+function function1() {
+  console.log("This is function one");
+}
+function function2() {
+  console.log("This is function two");
+  function1();
+}
+function2();
+
+function Booking_know() {
+  alert("Booking Is Not available");
+}
+
+function open() {
+  alert("Booking is not available . Thanks!");
+}
+function Login() {
+  alert("Log in Not Working");
+}
+var a = prompt("What is your name sir ?");
+if (a) {
+  alert("Thanks  " + a);
+} else {
+  alert("Sorry Sir!  " + a);
+}
+//==javascript toturials for beginners level//
+//==tags of html using in js==//
+/*
+document.write("<i> hello world </i>")
+document.write("<b> i want a programmer </b>")
+
+ var firstName = 'Sohail';
+ secondName = 'Sohail orakzai';
+ document.write(secondName);
+ 
+//this is table in js//
+console.table([1, 2, 3]);
+console.table([1, 2, 3]);
+//error log
+console.error("Something went wrong");
+
+const marks = 90;
+if (marks >= 90 && marks <= 100) {
+  console.log("Congratulation 🎉 You are excellent marks");
+} else if (marks >= 70 && marks <= 90) {
+  console.log("Congrates you are Good Marks");
+} else if (marks >= 50 && marks <= 70) {
+  console.log("Congrates you are Best Marks");
+} else if (marks >= 30 && marks <= 50) {
+  console.log("Your are just pass");
+}
+
+var x = 50;
+x > 40 ? console.log("True") : console.log("False");
+
+
+let day = 5;
+let dayName;
+
+switch (day) {
+  case 1:
+    dayName = 'Monday';
+    break;
+  case 2:
+    dayName = 'Tuesday';
+    break;
+  case 3:
+    dayName = 'Wednesday';
+    break;
+  case 4:
+    dayName = 'Thursday';
+    break;
+  case 5:
+    dayName = 'Friday';
+    break;
+  case 6:
+    dayName = 'Saturday';
+    break;
+  case 7:
+    dayName = 'Sunday';
+    break;
+  default:
+    dayName = 'Invalid day';
+}
+
+console.log(dayName); // Output: Wednesday
+
+const day = 2;
+let dayName;
+switch (day) {
+  case 1:
+    dayName = "Sunday";
+    break;
+  case 2:
+    dayName = "Monday";
+    break;
+  default:
+    dayName = "Error";
+}
+console.log(dayName);
+
+var x = 40;
+var y = 60;
+if (x > y) {
+  alert("X is Greater");
+} else {
+  alert("Y is Greater");
+}
+
+//==function in js==//
+function name() {
+  console.log("Hellow World");
+}
+name();
+name();
+name();
+
+
+function myFunction(a, b) {
+  document.write(a + b);
+}
+myFunction(10, 20);
+
+function fullName(fname, lName) {
+  const a = fname + " " + lName;
+  return a;
+}
+var fn = fullName("Muhammad", "Sohail");
+document.write(fn);
+
+
+//while loop==//
+var a = 1;
+while(a <= 10){
+  document.write("Become a fullstack developer <br>");
+  a = a + 1;
+}
+
+//do while loop//
+var a = 5;
+do{
+  document.write("Programmer <br>");
+  a = a + 1;
+}while(a <= 10);
+
+//for loop //
+for (let i = 0; i <= 10; i++) {
+  document.write("hello world <br>"); 
+}
+
+for (let b = 0; b <= 10; b++) {
+  if (b == 5) {
+    document.write("hey How are you");
+    continue;
+  }
+  document.write("Programmer <br> " + "  " +b);
+}
+
+
+for(let a = 1; a <= 100 ; a++){
+  if(a % 2 != 0){
+    document.write(a + "<br>");
+  }
+}
+*
+for (let a = 1; a <= 100; a = a + 10) {
+  // document.write(a + " <br>");
+  for (let b = a; b < a + 10; b++) {
+    document.write(b + " ");
+  }
+document.write("<br>")
+}
+
+
+for (let a = 1; a <= 5; a++) {
+  for (let b = 1; b <= a; b++) {
+    document.write(b + " ");
+  }
+  document.write("<br>")
+}
+
+for (let a = 1; a <= 10; a++) {
+  for (let b = 1; b <= a; b++) {
+    document.write(a);
+  }
+  document.write("<br>");
+}
+
+for(let a = 10 ; a >= 1; a--){
+  for(let b = a; b<=a ; b++){
+    document.write(a);
+  }
+  document.write("<br>")
+}
+
+
+const myArray = ["sohail" , "Wajid", "Amjid" , "tofail"];
+document.write(myArray[0]);
+*/
+
